cddee9400531f3884bb7ed692117c975e5eb851c
DHIONSON PLASTER MOREIRA <dhionsonmoreira@hotmail.com>
2022-12-09 21:46:50 -0300

    primeiro componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,13 +2,21 @@ import'./index.css'
 import  ReactDOM  from "react-dom";
 import React from 'react'
 
+import Primeiro  from './components/basicos/Primeiro';
 
 const tag = <label >Olá React!</label>
 
 
+// ReactDOM.render(
+//     <div>
+//         {tag}
+        
+//     </div>,
+//     document.getElementById('root')
+// )
 ReactDOM.render(
     <div>
-        {tag}
+        <Primeiro></Primeiro>
         
     </div>,
     document.getElementById('root')
